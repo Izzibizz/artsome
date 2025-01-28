8c7cd0d56d152abe264ef4365db08ec6f551ca94
izzibizz <contact@izabellind.com>
2025-01-28 16:00:42 +0100

    line in about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,9 @@
 
 export const About = () => {
   return (
-    <section className=" w-11/12 laptop:w-8/12 mx-auto flex flex-col h-full font-heading ">
-        <div className="flex flex-col laptop:flex-row absolute bottom-10 w-11/12">
-        <h3 className="font-fat text-4xl tablet:text-[50px] laptop:text-[130px] text-peach">About</h3>
+    <section className=" w-11/12 laptop:w-8/12 mx-auto flex flex-col h-10/12 font-heading ">
+        <div className="flex flex-col laptop:flex-row absolute bottom-20 laptop:bottom-10 w-11/12 gap-4">
+        <h3 className="font-fat text-4xl tablet:text-[50px] laptop:text-[130px] text-peach border-b-2 laptop:border-b-0 laptop:border-r-2 border-peach pr-2">About</h3>
 <p className="text-peach text-justify laptop:w-1/3">Artsome is a platform dedicated to showcasing contemporary artists from around the world. We highlight a wide range of artistic styles and mediums, offering a space to discover new and exciting talent. We provide you with the artist's profile, work, and websites to learn more. </p>
 </div>
         </section>
